5b9920ed5783473ee4a16d9b9cee1fd8a2bf2a60
jy_wee <toya_@live.co.kr>
2023-06-22 22:45:09 +0900

    ui

--- Source/Ue5study/Snake/SnakeActor.cpp
+++ Source/Ue5study/Snake/SnakeActor.cpp
@@ -2,6 +2,8 @@
 
 
 #include "SnakeActor.h"
+#include "SnakeGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ASnakeActor::ASnakeActor()
@@ -9,7 +11,32 @@ ASnakeActor::ASnakeActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//Tags.Add("SnakeActor");
+
+	Tags.Add("SnakeActor");
+}
+
+ASnakeGameMode* ASnakeActor::GetSnakeGameMode()
+{
+	AGameModeBase* GameModePtr = UGameplayStatics::GetGameMode(GetWorld());
+	if (nullptr == GameModePtr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%S(%u)> if (nullptr == GameMode)"), __FUNCTION__, __LINE__);
+		return nullptr;
+	}
+	if (false == GameModePtr->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Error, TEXT("%S(%u)> if (nullptr == GameMode)"), __FUNCTION__, __LINE__);
+		return nullptr;
+	}
+	
+	ASnakeGameMode* SnakeGameMode = Cast<ASnakeGameMode>(GameModePtr);
+	if (nullptr == SnakeGameMode && false == SnakeGameMode->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Error, TEXT("%S(%u)> nullptr == SnakeGameMode && false == SnakeGameMode->IsValidLowLevel"), __FUNCTION__, __LINE__);
+		return nullptr;
+	}
+
+	return SnakeGameMode;
 }
 
 // Called when the game starts or when spawned
@@ -17,6 +44,15 @@ void ASnakeActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	AGameModeBase* GameModePtr = UGameplayStatics::GetGameMode(GetWorld());
+	if (nullptr == GameModePtr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%S(%u)> if (nullptr == GameMode)"), __FUNCTION__, __LINE__);
+		return;
+	}
+
+	AddTickPrerequisiteActor(GameModePtr);
+
 }
 
 // Called every frame
